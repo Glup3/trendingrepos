e87f0c1d4bc262c8a97d4af83fc293730e4ce831
Phuc Tran <glup3.tran@gmail.com>
2025-05-01 14:01:50 +0200

    move search code

--- backend/api/client.go
+++ backend/api/client.go
@@ -1,15 +1,10 @@
 package api
 
 import (
-	"context"
-	"fmt"
 	"net/http"
-	"strings"
 
 	"github.com/Khan/genqlient/graphql"
 )
-
-const pageSize = 100
 
 type APIClient struct {
 	gClient graphql.Client
@@ -40,55 +35,4 @@ func NewAPIClient(apiKey string) *APIClient {
 	return &APIClient{
 		gClient: c,
 	}
-}
-
-type Repo struct {
-	Id              string
-	Stars           int
-	Description     string
-	NameWithOwner   string
-	PrimaryLanguage string
-}
-
-type QueryArgs struct {
-	MinStars         int
-	MaxStars         int
-	Languages        []string
-	IgnoredLanguages []string
-}
-
-func (c *APIClient) SearchRepos(ctx context.Context, cursor string, queryArgs QueryArgs) ([]Repo, error) {
-	repos := make([]Repo, 0, pageSize)
-
-	resp, err := searchRepos(ctx, c.gClient, buildQuery(queryArgs), pageSize, cursor)
-	if err != nil {
-		return nil, err
-	}
-
-	for _, edge := range resp.Search.Edges {
-		repo := edge.Node.(*searchReposSearchSearchResultItemConnectionEdgesSearchResultItemEdgeNodeRepository)
-		repos = append(repos, Repo{
-			Id:              repo.Id,
-			Stars:           repo.StargazerCount,
-			Description:     repo.Description,
-			NameWithOwner:   repo.NameWithOwner,
-			PrimaryLanguage: repo.PrimaryLanguage.Name,
-		})
-	}
-
-	return repos, nil
-}
-
-func buildQuery(args QueryArgs) string {
-	var b strings.Builder
-	b.WriteString("is:public ")
-	b.WriteString("fork:true ")
-	fmt.Fprintf(&b, "stars:%d..%d ", args.MinStars, args.MaxStars)
-	for _, lang := range args.Languages {
-		fmt.Fprintf(&b, "language:%s ", lang)
-	}
-	for _, lang := range args.IgnoredLanguages {
-		fmt.Fprintf(&b, "-language:%s ", lang)
-	}
-	return b.String()
 }
