7b142cd514ad62c3de87b4793c1b1b78a8c92125
Phuc Tran <glup3.tran@gmail.com>
2025-05-22 12:42:41 +0200

    ui: use npm instead of pnpm

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -2,7 +2,6 @@ import type { QueryClient } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import {
   HeadContent,
-  Link,
   Outlet,
   Scripts,
   createRootRouteWithContext,
